77c6b467975b879653e4dc2baf7c3a9d8d945164
Jérémie St-Pierre Robitaille <jeremiesrobitaille@gmail.com>
2020-09-09 19:45:00 -0400

    dashboard init

---
+++ dashboard/src/pages/Clients.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="content">
+    <div class="md-layout">
+      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
+        <md-card>
+          <md-card-header data-background-color="green">
+            <h4 class="title">Clients</h4>
+            <!-- <p class="category">Here is a subtitle for this table</p> -->
+          </md-card-header>
+          <md-card-content>
+            <clients-table table-header-color="green"></clients-table>
+          </md-card-content>
+        </md-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ClientsTable from "@/components/Clients/Table";
+
+export default {
+  components: {
+    ClientsTable
+  }
+};
+</script>
+
+
