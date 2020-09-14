f2b60ac9d5789c85cafb6fe0c9e6fd1439562a68
Jérémie St-Pierre Robitaille <jeremiesrobitaille@gmail.com>
2020-09-14 17:08:03 -0400

    vuetify

---
+++ vuetify_dashboard/src/views/dashboard/Clients/Select.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-select
+    :items="options"
+    v-model="currentValue"
+    :label="label"
+    item-text="name"
+    item-value="id"
+    return-object />
+</template>
+
+<script>
+import { Controller } from '@/store'
+export default {
+  props: {
+    value: {
+      type: Object,
+      default: () => {}
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    currentValue: {
+      set (value) {
+        this.$emit('input', value)
+      },
+      get () {
+        return this.value
+      }
+    },
+    options () {
+      return Controller.all()
+    }
+  },
+  mounted () {
+    Controller.$fetchControllers()
+  }
+}
+</script>
+
+<style>
+
+</style>
